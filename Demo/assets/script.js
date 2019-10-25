16f1f62ee62444b85cc611c7e03dbaa897301718
AM-77 <amine.griche77@gmail.com>
2019-10-25 02:08:54 +0200

    added generatePasswordWithChars

--- Demo/assets/script.js
+++ Demo/assets/script.js
@@ -1,6 +1,6 @@
 let p = new pGen()
 
-let generated = document.querySelector("p.generated")
+let generated = document.querySelector("textarea.generated")
 let generate_btn = document.querySelector("button.generate")
 
 let lowercase = document.getElementById("lowercase")
@@ -10,5 +10,15 @@ let symbols = document.getElementById("symbols")
 let length = document.getElementById("length")
 
 generate_btn.addEventListener("click", ()=>{
-    generated.innerHTML = p.generatePassword(lowercase.checked, uppercase.checked, numbers.checked, symbols.checked, length.value)
+    generated.value = p.generatePassword(lowercase.checked, uppercase.checked, numbers.checked, symbols.checked, length.value)
+})
+
+
+let chars_to_use = document.querySelector("textarea.chars")
+let length_with_chars = document.getElementById("length_with_chars")
+let generated_with_chars = document.querySelector("textarea.generated_with_chars")
+let generate_with_chars_btn = document.querySelector("button.generate_with_chars")
+
+generate_with_chars_btn.addEventListener("click", () => {
+    generated_with_chars.value = p.generatePasswordWithChars(chars_to_use.value, length_with_chars.value)
 })
